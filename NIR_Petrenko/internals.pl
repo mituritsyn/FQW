f5720ac83e05c6ba169d3991af54dece8c53a4ac
Valentina <ikherty@gmail.com>
2020-12-27 02:16:01 +0300

    some fixes

---
+++ NIR_Petrenko/internals.pl
@@ -0,0 +1,66 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:aruco_detect/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:frame/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:map/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:motor/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pix/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:quad1/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:quad2/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ris1/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ris2/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ris3/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ris4/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:stack/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:time/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:topic/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/lst:1/;
+$ref_files{$key} = "$dir".q|NIR_Petrenko.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
